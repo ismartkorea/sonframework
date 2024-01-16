aac668134f214b3afbb43879a4b38ef995da5a4f
DESKTOP-5FTFAG9\hibiznet <ismartkorea@hotmail.com>
2024-01-16 18:20:46 +0900

    - 수정.

--- src/main/java/sonframework/test/web/TestController.java
+++ src/main/java/sonframework/test/web/TestController.java
@@ -64,9 +64,75 @@ public class TestController {
 			 model.addAttribute("message",
 			 egovMessageSource.getMessage("fail.common.login")); 
 			 return "uat/uia/EgovLoginUsr"; 
-		 }		
+		 }
+		 
+		 	/**
+		 	 * 페이징 관련.
+		 	 *
+			PaginationInfo paginationInfo = new PaginationInfo();
+
+			paginationInfo.setCurrentPageNo(boardVO.getPageIndex());
+			paginationInfo.setRecordCountPerPage(boardVO.getPageUnit());
+			paginationInfo.setPageSize(boardVO.getPageSize());
+			
+			boardVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+			boardVO.setLastIndex(paginationInfo.getLastRecordIndex());
+			boardVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+			
+			Map<String, Object> map = bbsMngService.selectBoardArticles(boardVO, vo.getBbsAttrbCode());
+			int totCnt = Integer.parseInt((String) map.get("resultCnt"));
+
+			paginationInfo.setTotalRecordCount(totCnt);
+			
+			model.addAttribute("resultList", map.get("resultList"));
+			model.addAttribute("resultCnt", map.get("resultCnt"));
+			//model.addAttribute("boardVO", boardVO);
+			//model.addAttribute("brdMstrVO", master);
+			model.addAttribute("paginationInfo", paginationInfo);			
+			*/
 		
 		
 		return "test/blog/page";
+	}
+	
+	@RequestMapping(value = "/test/blog/post/page.do")
+	public String blogPostPage(HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
+		
+		LOGGER.debug(">>> Called blog !!! <<<");
+		 // 미인증 사용자에 대한 보안처리 
+		 Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated(); 
+		 if(!isAuthenticated) {
+			 model.addAttribute("message",
+			 egovMessageSource.getMessage("fail.common.login")); 
+			 return "uat/uia/EgovLoginUsr"; 
+		 }
+		 
+		 	/**
+		 	 * 페이징 관련.
+		 	 *
+			PaginationInfo paginationInfo = new PaginationInfo();
+
+			paginationInfo.setCurrentPageNo(boardVO.getPageIndex());
+			paginationInfo.setRecordCountPerPage(boardVO.getPageUnit());
+			paginationInfo.setPageSize(boardVO.getPageSize());
+			
+			boardVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+			boardVO.setLastIndex(paginationInfo.getLastRecordIndex());
+			boardVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+			
+			Map<String, Object> map = bbsMngService.selectBoardArticles(boardVO, vo.getBbsAttrbCode());
+			int totCnt = Integer.parseInt((String) map.get("resultCnt"));
+
+			paginationInfo.setTotalRecordCount(totCnt);
+			
+			model.addAttribute("resultList", map.get("resultList"));
+			model.addAttribute("resultCnt", map.get("resultCnt"));
+			//model.addAttribute("boardVO", boardVO);
+			//model.addAttribute("brdMstrVO", master);
+			model.addAttribute("paginationInfo", paginationInfo);			
+			*/
+		
+		
+		return "test/blog/postPage";
 	}	
 }
